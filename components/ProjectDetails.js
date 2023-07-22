3ea6d669d08362032b1fdea7b889783664c49856
Phillip Anerine <paner225@gmail.com>
2023-07-21 23:51:25 -0400

    big progress

---
+++ components/ProjectDetails.js
@@ -0,0 +1,20 @@
+export const projects = {
+    NookLook: {
+        title: "NookLook",
+        shortDescription: "A web game where users try to name the most Animal Crossing villagers they can. Built with React and the external Nookipedia API. Here I learned how to optimize performance with React hooks and manage more complex state with useReducer and local storage. ",
+        tech: ['/images/react.svg', '/images/html.svg','/images/css.svg'],
+        images: ['/projects/nooklook/s1.png', '/projects/nooklook/s2.png'],
+    },
+    Coordinote: {
+        title: "Coordinote",
+        shortDescription: "A full stack web application for users to plan out when to meet up based on when everyone is available. This is done with an interactive time selector and calendar. This was done with a group using the MERN stack, and firebase for authentication. I was mostly responsible for the frontend and AWS deployment. I learned a lot about fullstack development and how to work with a team.",
+        tech: ['/images/react.svg', '/images/html.svg','/images/css.svg', '/images/nodejs.svg', '/images/mongodb.svg', '/images/express.svg'],
+        images: ['/projects/coordinote/s1.png', '/projects/coordinote/s2.png', '/projects/coordinote/s3.png', '/projects/coordinote/s4.png'],
+    },
+    CyclSafe: {
+        title: "CyclSafe",
+        shortDescription: "A full stack web application that allows users to report provides information on a map for bikers. Allows users to rate different areas, report infrastructure issues, and view other users' reports. Has a route suggestion feature based on user ratings and Google Map information. This was done with a group using jQuery, Node.js, MongoDB, and Google Maps API. I was mostly responsible for interactive frontend client code. I learned a lot about the fundamentals of web development and project management.",
+        tech: ['/images/html.svg','/images/css.svg', '/images/javascript.svg','/images/nodejs.svg', '/images/mongodb.svg'],
+        images: ['/projects/cyclsafe/s1.png', '/projects/cyclsafe/s2.png', '/projects/cyclsafe/s3.png', '/projects/cyclsafe/s4.png'],
+    }
+}

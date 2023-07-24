d8414ef06afb6e29860543915752f2260b533d95
phill52 <paner225@gmail.com>
2023-07-24 11:14:42 -0400

    fixed reponsive issue

--- components/Project.jsx
+++ components/Project.jsx
@@ -8,10 +8,10 @@ const Project = ({project}) => {
             <a href={project.link} target='_blank' rel='noopener'>
                 <h3 className='text-pearl lg:text-3xl text-lg font-bold w-auto'>{project.title}</h3>
             </a>
-            <div className='absolute top-[52px] w-[90%] h-[315px] lg:w-[560px] lg:h-[315px] sm:w-[200px] sm:h-[113px] self-center mx-auto'>
+            <div className='absolute top-[52px] w-[90%] lg:h-[315px] h-[150px] self-center mx-auto'>
                 <SimpleImageSlider width='100%' height='100%' images={project.images} showBullets={true} showNavs={true} navStyle={2} navSize={30} navMargin={10} />
             </div>
-            <p className='text-pearl text-lg pt-[21rem]'> {project.shortDescription}
+            <p className='text-pearl text-lg lg:pt-[21rem] pt-[11rem]'> {project.shortDescription}
             <br/>
             Technologies used: {project.tech.map((item)=>
                 {return <Image src={item} key={item.split('.')} height={50} width={50} className='inline-block pl-2' alt={`${item}`}/>}

046de01cc713f195e0759325112fdbfa5855789a
phill52 <paner225@gmail.com>
2023-07-24 15:16:42 -0400

    underlined title

--- components/Project.jsx
+++ components/Project.jsx
@@ -6,7 +6,7 @@ const Project = ({project}) => {
     return (
         <div className='flex flex-col bg-darkocean justify-center items-center px-7 rounded-2xl py-4 w-full relative'>
             <a href={project.link} target='_blank' rel='noopener'>
-                <h3 className='text-pearl lg:text-3xl text-lg font-bold w-auto'>{project.title}</h3>
+                <h3 className='text-pearl lg:text-3xl text-lg font-bold w-auto underline'>{project.title}</h3>
             </a>
             <div className='absolute top-[52px] w-[90%] lg:h-[315px] h-[150px] self-center mx-auto'>
                 <SimpleImageSlider width='100%' height='100%' images={project.images} showBullets={true} showNavs={true} navStyle={2} navSize={30} navMargin={10} />

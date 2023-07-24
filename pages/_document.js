0fe6c650bb6418f77d9c6222e502603049f5153b
phill52 <paner225@gmail.com>
2023-07-24 12:47:08 -0400

    chnaged icon and description

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head />
         <title>Phillip Anerine | Full Stack Developer </title>
-        <meta name="description" content="Hello, strange to see you in the metadata. I'm Phillip Anerine, currently a Computer Science major at
+        <meta name="description" content="Hello, I'm Phillip Anerine, currently a Computer Science major at
         Stevens Institute of Technology, and aspiring full stack developer." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" />
       <body>
         <Main />
         <NextScript />
